f0c134793b2e45560c57c277225a565c4f6825f0
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-09-29 13:07:29 -0500

    Tutorial up to lession, Objects

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-let name = "Jill";
-console.log(name);
+
+//below is object literal
+let person = {
+    name: 'Jill',
+    age: 30
+};
+
+
+//dot notation
+person.name = 'Jan';
+
+//Bracket Notation
+let selection = 'name';
+person[selection] = 'Mary';
+
+console.log(person.name);
