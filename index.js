da80b9a4bd7c8aad70dcc17c8b58277fb672ee12
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-10-05 14:33:40 -0500

    switch case

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-let hour = 13;
 
-if (hour >= 6 && hour < 12)
-    console.log('Good Morning!');
-else if (hour >= 12 && hour < 18)
-    console.log('Good Afternoon!');
-else
-    console.log('Good Evening!');
+let role = 'moderator';
 
+switch (role) {
+    case 'guest':
+        console.log('Guest User');
+        break;
+
+    case 'moderator':
+        console.log('Moderator User');
+        break;
+
+    default:
+        console.log('Unknown User');
+}
+
+if (role === 'guest') console.log('Guest');
+else if (role === 'moderator') console.log('Moderator');
+else console.log('Unknown User');
