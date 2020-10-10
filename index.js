718447c413fd936a01179759964f43ca35854a2c
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-10-10 18:47:54 -0500

    break continue

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 
-// for (let i = 0; i <= 5; i++) {
-//     if (i % 2 !== 0) console.log(i);
-// }
+let i = 0;
+while (i <= 10) {
+    // if (i === 5) break;
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    }
 
-// let i = 9;
-// while (i <= 5) {
-//     if (i % 2 !== 0) console.log(i);
-//     i++;
-// }
-
-let i = 9;
-do {
-    if (i % 2 !== 0) console.log(i);
+    console.log(i);
     i++;
-} while (i <= 5);
+}
