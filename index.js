b4f5581e7db11cbecd3bd064489ddcec131e9a44
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-10-12 16:31:34 -0500

    fizzbuzz

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-const output = fizzBuzz(false);
+const output = fizzBuzz(4);
 console.log(output);
 
 function fizzBuzz(input) {
     if (typeof input !== 'number')
         return NaN;
 
-    if ((input % 3 === 0) && (input % 5 === 0))
+    if ((input % 2 === 0) && (input % 10 === 0))
         return 'FizzBuzz';
 
-    if (input % 3 === 0)
+    if (input % 2 === 0)
         return 'Fizz';
 
-    if (input % 5 === 0)
+    if (input % 10 === 0)
         return 'Buzz';
 
     return input;
