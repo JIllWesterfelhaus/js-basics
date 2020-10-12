026ae1710559088ee25bca838bc341d52ca7b2b4
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-10-12 17:33:29 -0500

    trying odd even

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 
-const output = fizzBuzz(4);
-console.log(output);
+// const output = fizzBuzz(4);
+// console.log(output);
 
-function fizzBuzz(input) {
+// function fizzBuzz(input) {
+//     if (typeof input !== 'number')
+//         return NaN;
+
+//     if ((input % 2 === 0) && (input % 10 === 0))
+//         return 'FizzBuzz';
+
+//     if (input % 2 === 0)
+//         return 'Fizz';
+
+//     if (input % 10 === 0)
+//         return 'Buzz';
+
+//     return input;
+// }
+
+const output = oddEven(4); {
+    console.log(output);
+
+    if (input % 2 === 0)
+        return 'Even';
+
+    if (input % 1 === 0)
+        return 'Odd';
+
     if (typeof input !== 'number')
         return NaN;
 
-    if ((input % 2 === 0) && (input % 10 === 0))
-        return 'FizzBuzz';
-
-    if (input % 2 === 0)
-        return 'Fizz';
-
-    if (input % 10 === 0)
-        return 'Buzz';
-
     return input;
-
 }
+
 
