bc63e3bc11143838329fe65c07c00ede886bb677
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-10-14 16:20:32 -0500

    demerit points

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 
-showNumbers(10);
+// Speed Limit = 70
+// 5 -> 1 point
+// Math.floor(1.3)
+// 12 points -> suspended
 
-function showNumbers(limit) {
-    for (let i = 0; i <= limit; i++) {
+checkSpeed(130);
 
-        const message = (i % 2 === 0) ? 'EVEN' : 'ODD';
-        console.log(i, message);
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
 
-
+    if (speed < speedLimit + kmPerPoint) {
+        console.log('Ok');
+        return;
     }
-}
+    const points = Math.floor(speed - speedLimit) /
 
 
 
