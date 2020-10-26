54720367a45ba3f2fb66861d84a5b20a01c18754
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-10-26 18:22:36 -0500

    dynamic nature of objects

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 
+const circle = {
+    radius: 1
+};
 
-function createCircle(radius) {
-    return {
-        radius,
-        draw() {
-            console.log('draw');
-        }
-    };
-}
+circle.color = 'yellow';
+circle.draw = function () { }
 
-const circle1 = createCircle(1);
-console.log(circle1);
+delete circle.color;
+delete circle.draw;
 
-const circle2 = createCircle(2);
-console.log(circle2);
-
-
-
-const myCircle = createCircle(1);
-
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function () {
-        console.log('draw');
-    }
-}
-
-const circle = new Circle(1);
-
+console.log(circle);
